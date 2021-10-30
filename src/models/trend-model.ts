0ba4cc6043b63ac9bbd3d2610d0f2d22bc71d43e
punjira <saeedwmail@gmail.com>
2021-10-30 19:38:20 +0330

    add trend model

---
+++ src/models/trend-model.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+interface Trend {
+     ticker: string;
+     interval: string;
+     redisKey: string;
+     status: boolean;
+     direction?: 1 | -1;
+     strength?: boolean;
+     started_at?: number;
+     dollar_volume?: number;
+}
+
+const trendSchema = new mongoose.Schema<Trend>(
+     {
+          ticker: {
+               type: String,
+               required: true,
+               unique: false,
+          },
+          interval: {
+               type: String,
+               required: true,
+               unique: false,
+          },
+          redisKey: {
+               type: String,
+               required: true,
+               unique: true,
+          },
+          status: {
+               type: Boolean,
+               required: true,
+          },
+          direction: Number,
+          strength: Boolean,
+          started_at: Number,
+          dollar_volume: Number,
+     },
+     { timestamps: true }
+);
+
+const TrendModel = mongoose.model<Trend>('trend', trendSchema);
+
+export { TrendModel, Trend };
