137ec8d47bd0df11ee858d65d6c06e9e6800fcb0
punjira <saeedwmail@gmail.com>
2021-11-02 11:02:48 -0400

    add price controllers

---
+++ src/controllers/price-controller.ts
@@ -0,0 +1,37 @@
+import { Price, PriceModel } from '../models/price-model';
+
+export function upsertPrice(price: Price): Promise<boolean> {
+     return new Promise((resolve, reject) => {
+          PriceModel.updateOne(
+               {
+                    ticker: price.ticker,
+               },
+               price,
+               { upsert: true },
+               function (err, res) {
+                    if (err) {
+                         return reject(err);
+                    }
+                    /**
+                     * after all, what could go wrong?
+                     */
+                    return resolve(true);
+               }
+          );
+     });
+}
+
+export function getLatestAvailableDate(ticker: string): Promise<number | null> {
+     return new Promise((resolve, reject) => {
+          PriceModel.findOne(
+               { ticker: ticker },
+               { date: 1 },
+               function (err, res) {
+                    if (err) {
+                         return reject(err);
+                    }
+                    return resolve(res?.date || null);
+               }
+          );
+     });
+}
