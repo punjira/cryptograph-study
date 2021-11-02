56e248c98b5bff55cd2d578dc759dc6e6658d7f7
punjira <saeedwmail@gmail.com>
2021-11-02 11:03:39 -0400

    add candlestick helpers

---
+++ src/helpers/candlestick-helper.ts
@@ -0,0 +1,21 @@
+export const reversal_positives: string[] = [
+     'hammer',
+     'inverted_hammer',
+     'piercing_line',
+     'morning_star',
+     'morning_doji_star',
+     'harami',
+     'harami_cross',
+     'engulfing',
+];
+
+export const reversal_negatives: string[] = [
+     'shooting_star',
+     'evening_star',
+     'evening_doji_star',
+     'hanging_man',
+     'dark_cloud_cover',
+     'engulfing',
+     'harami',
+     'harami_cross',
+];
