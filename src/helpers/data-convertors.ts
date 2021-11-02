7fb27b5e38e5d2de5890abc04a5a581431a45e95
punjira <saeedwmail@gmail.com>
2021-11-02 11:03:15 -0400

    update data convertors

--- src/helpers/data-convertors.ts
+++ src/helpers/data-convertors.ts
@@ -1,5 +1,11 @@
 import { Candlestick } from '../models/candlestick-model';
-import { CandlestickUpdateMessage } from '../nats/sbscription';
+import { Indicator } from '../models/indicator-model';
+import { Price } from '../models/price-model';
+import {
+     CandlestickUpdateMessage,
+     IndicatorUpdateMessage,
+     NewCandlestickEvent,
+} from '../nats/sbscription';
 
 export function createCandlestickObject(
      data: CandlestickUpdateMessage
@@ -10,5 +16,24 @@ export function createCandlestickObject(
           redis_key: data.key,
           pattern: data.pattern,
           location: data.location,
+     };
+}
+
+export function createIndicatorObject(data: IndicatorUpdateMessage): Indicator {
+     return {
+          base_indicator: data.base_indicator,
+          interval: data.interval,
+          location: data.location,
+          redis_key: data.key,
+          ticker: data.ticker,
+          details: data.type,
+     };
+}
+
+export function createPriceFromCandlestick(candle: NewCandlestickEvent): Price {
+     return {
+          date: candle.data[0],
+          ticker: candle.ticker.split('-')[0],
+          price: (candle.data[1] + candle.data[4]) / 2,
      };
 }
