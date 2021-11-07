6a27dd1eca4a2fe95814e43155d598db642ff677
punjira <saeedwmail@gmail.com>
2021-11-07 05:38:40 -0500

    change connection flow

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-require('./database/mongo');
-require('./nats/sbscription');
+import { MongoConenct } from './database/mongo';
+import { createStudySchedule } from './jobs/manager';
+import { createSubscriptions } from './nats/sbscription';
 import express from 'express';
 const app = express();
 import cors from 'cors';
@@ -7,11 +8,18 @@ import bodyParser from 'body-parser';
 app.use(cors());
 app.use(bodyParser({ extended: true }));
 
+MongoConenct(() => {
+     createStudySchedule;
+     createSubscriptions();
+});
+
 import TrendRouter from './routes/trend-routes';
 import CandlestickRouter from './routes/candlestick-routes';
+import IndicatorRouter from './routes/indicator-routes';
 
 app.use('/trend', TrendRouter);
 app.use('/candlestick', CandlestickRouter);
+app.use('/indicator', IndicatorRouter);
 
 app.listen(process.env.PORT, () => {
      console.log('study server is up on port ,', process.env.PORT);
