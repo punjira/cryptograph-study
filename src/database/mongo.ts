53ff4e266e9adfcc044e633e1f4cf622632e1c5d
punjira <saeedwmail@gmail.com>
2021-11-07 05:38:20 -0500

    change mongoose connection helper

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -2,17 +2,17 @@ import { DatabaseConnectionError } from '@cryptograph-app/error-handlers';
 import mongoose from 'mongoose';
 import { logger, LOG_LEVELS } from '../../winston';
 
-let connected: boolean = false;
-
-(async function () {
-     if (connected) {
-          return;
-     }
-     await mongoose.connect(`${process.env.MONGO_URL}/history`);
+export const MongoConenct = function (callback) {
+     console.log('creating mongo db connection');
+     mongoose.connect(`${process.env.MONGO_URL}`, {
+          connectTimeoutMS: 30000,
+          keepAlive: true,
+          dbName: 'study',
+     });
      const db = mongoose.connection;
      db.once('open', () => {
           console.log('connection to mongo db created');
-          connected = true;
+          callback();
      });
      db.on('error', (err) => {
           logger(
@@ -20,6 +20,24 @@ let connected: boolean = false;
                'error connecting to database , error description: ' + err,
                'database/mongo.ts'
           );
+          console.log('=======================================');
+          console.log('mongo connections error, Error: ', err);
           throw new DatabaseConnectionError();
      });
-})();
+     db.on('disconnected', (thing) => {
+          console.log('========================================');
+          console.log('mongo connection disconnected, The thing: ', thing);
+          // mongoose.connect(`${process.env.MONGO_URL}/study`, {
+          //      connectTimeoutMS: 3000,
+          //      keepAlive: true,
+          // });
+     });
+     db.on('disconnecting', (thing) => {
+          console.log('========================================');
+          console.log('mongo connection disconnecting, The thing: ', thing);
+     });
+     db.on('close', (thing) => {
+          console.log('========================================');
+          console.log('mongo connection disconnecting, The thing: ', thing);
+     });
+};
