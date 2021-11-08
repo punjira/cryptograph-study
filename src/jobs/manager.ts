6c2a0ec4a498e5ad8be53f73657afac3eed49ed1
punjira <saeedwmail@gmail.com>
2021-11-08 04:07:00 -0500

    schedule study

--- src/jobs/manager.ts
+++ src/jobs/manager.ts
@@ -1,9 +1,18 @@
 import schedule from 'node-schedule';
-import { tickerQueue } from './caller';
+import { studyQueue, tickerQueue } from './caller';
 
 import { getAllExchanges } from '../controllers/exchange-controller';
 
-export const createStudySchedule = (function () {
+const frames = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '1d'];
+
+async function createInitialData() {
+     for (let one of frames) {
+          await tickerQueue(one);
+     }
+}
+
+export const createStudySchedule = (async function () {
+     await createInitialData();
      getAllExchanges().then((data) => {
           const m_1_j = schedule.scheduleJob('*/2 * * * *', () => {
                tickerQueue('1m');
@@ -13,6 +22,12 @@ export const createStudySchedule = (function () {
           });
           const m_5_j = schedule.scheduleJob('*/6 * * * *', () => {
                tickerQueue('5m');
+          });
+          const study = schedule.scheduleJob('*/1 * * * *', () => {
+               console.log('running study');
+               for (let one of frames) {
+                    studyQueue(one);
+               }
           });
      });
      return {
