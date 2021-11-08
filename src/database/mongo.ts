f9f5492182b7f31d64fb9748532819cb83fa4c87
punjira <saeedwmail@gmail.com>
2021-11-08 04:00:41 -0500

    update mongoose connection helper

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,4 +1,3 @@
-import { DatabaseConnectionError } from '@cryptograph-app/error-handlers';
 import mongoose from 'mongoose';
 import { logger, LOG_LEVELS } from '../../winston';
 
@@ -6,6 +5,7 @@ export const MongoConenct = function (callback) {
      console.log('creating mongo db connection');
      mongoose.connect(`${process.env.MONGO_URL}`, {
           connectTimeoutMS: 30000,
+          socketTimeoutMS: 30000,
           keepAlive: true,
           dbName: 'study',
      });
@@ -20,24 +20,15 @@ export const MongoConenct = function (callback) {
                'error connecting to database , error description: ' + err,
                'database/mongo.ts'
           );
-          console.log('=======================================');
-          console.log('mongo connections error, Error: ', err);
-          throw new DatabaseConnectionError();
      });
-     db.on('disconnected', (thing) => {
-          console.log('========================================');
-          console.log('mongo connection disconnected, The thing: ', thing);
-          // mongoose.connect(`${process.env.MONGO_URL}/study`, {
-          //      connectTimeoutMS: 3000,
-          //      keepAlive: true,
-          // });
+     db.on('disconnected', () => {
+          console.log('mongo connection disconnected');
+          mongoose.connect(`${process.env.MONGO_URL}`, {
+               connectTimeoutMS: 30000,
+               socketTimeoutMS: 30000,
+               keepAlive: true,
+               dbName: 'study',
+          });
      });
-     db.on('disconnecting', (thing) => {
-          console.log('========================================');
-          console.log('mongo connection disconnecting, The thing: ', thing);
-     });
-     db.on('close', (thing) => {
-          console.log('========================================');
-          console.log('mongo connection disconnecting, The thing: ', thing);
-     });
+     db.on('close', (thing) => {});
 };
