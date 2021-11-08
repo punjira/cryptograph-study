a7f708c1164c192fc11ce86e379bd7b69af9dc7a
punjira <saeedwmail@gmail.com>
2021-11-08 11:39:31 -0500

    fix logic

--- src/signals/p_1_st_g.ts
+++ src/signals/p_1_st_g.ts
@@ -5,15 +5,15 @@ import { promisedGetTrendForAsset as d } from '../controllers/trend.controllers'
 import { promisedGetLatestForTicker as e } from '../controllers/indicator-controller';
 import { getLatestAvailablePrice as f } from '../controllers/price-controller';
 import { promisedCandlestickOnInterval as g } from '../controllers/candlestick-controller';
-import { createSignalObject as l } from '../helpers/signal-helpers';
+import { createSignalObject as l, frameMap } from '../helpers/signal-helpers';
 import {
      reversal_negatives,
      reversal_positives,
 } from '../helpers/candlestick-helper';
 import { addSignal } from '../controllers/signal-controller';
 
-function laps(_: number, c: number): number {
-     return _ - c * 60000;
+function laps(_: number, c: number, k: string): number {
+     return _ - c * frameMap[k];
 }
 
 export default async function p1R(one: string, ol: string) {
@@ -26,13 +26,13 @@ export default async function p1R(one: string, ol: string) {
           const i8 = await f(m1); // last price date
           if (!i9) return;
           if (!i8) return;
-          if (laps(i8.date, 3) > i9.location) return;
+          if (laps(i8.date, 3, ol) > i9.location) return;
           if (k.direction === 1 && i9.details === 'overbought') return;
           if (k.direction === -1 && i9.details === 'oversold') return;
           const hk2 = await g(m1, ol); // candlesticks
           const rt = hk2.filter((el) => {
                if (
-                    laps(i8.date, 3) < el.location &&
+                    laps(i8.date, 3, ol) < el.location &&
                     el.direction === k.direction
                ) {
                     if (k.direction === 1) {
