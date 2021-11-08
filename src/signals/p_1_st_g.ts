90ac4cb14c04e891a52168f4e0d53c424bd768c6
punjira <saeedwmail@gmail.com>
2021-11-08 04:05:58 -0500

    update signal

--- src/signals/p_1_st_g.ts
+++ src/signals/p_1_st_g.ts
@@ -3,40 +3,61 @@
  */
 import { promisedGetTrendForAsset as d } from '../controllers/trend.controllers';
 import { promisedGetLatestForTicker as e } from '../controllers/indicator-controller';
-import { getLatestAvailableDate as f } from '../controllers/price-controller';
+import { getLatestAvailablePrice as f } from '../controllers/price-controller';
 import { promisedCandlestickOnInterval as g } from '../controllers/candlestick-controller';
+import { createSignalObject as l } from '../helpers/signal-helpers';
 import {
      reversal_negatives,
      reversal_positives,
 } from '../helpers/candlestick-helper';
+import { addSignal } from '../controllers/signal-controller';
+
+function laps(_: number, c: number): number {
+     return _ - c * 60000;
+}
 
 export default async function p1R(one: string, ol: string) {
      try {
           const m1 = one;
-          const k = await d(m1, ol);
+          const k = await d(m1, ol); // trend
           if (!k) return;
           if (k.started_at < 50) return;
-          const i9 = await e(m1, ol, 'stochastic');
-          const i8 = await f(m1);
-          const hk2 = await g(m1, ol);
+          const i9 = await e(m1, ol, 'stochastic'); // indicator
+          const i8 = await f(m1); // last price date
           if (!i9) return;
           if (!i8) return;
-          if (laps(i8, 3) > i9.location) return;
+          if (laps(i8.date, 3) > i9.location) return;
           if (k.direction === 1 && i9.details === 'overbought') return;
           if (k.direction === -1 && i9.details === 'oversold') return;
-          const rt = hk2.filter(
-               (el) =>
-                    (k.direction === 1
-                         ? reversal_positives.includes[el.pattern]
-                         : reversal_negatives.includes[el.pattern]) &&
-                    el.direction == -1 * k.direction
-          );
-          console.log(rt, m1, ol);
+          const hk2 = await g(m1, ol); // candlesticks
+          const rt = hk2.filter((el) => {
+               if (
+                    laps(i8.date, 3) < el.location &&
+                    el.direction === k.direction
+               ) {
+                    if (k.direction === 1) {
+                         if (reversal_positives.includes(el.pattern)) {
+                              return el;
+                         }
+                    } else {
+                         if (reversal_negatives.includes(el.pattern)) {
+                              return el;
+                         }
+                    }
+               }
+          });
+          if (rt.length) {
+               const m4 = l(
+                    `${m1}-${ol}`,
+                    i8.price,
+                    i8.date,
+                    k.direction === 1 ? 'long' : 'short',
+                    'p_1_st_g',
+                    rt.filter((el) => el._id)
+               );
+               await addSignal(m4);
+          }
      } catch (err) {
           console.log('error while running standard process, Error: ', err);
      }
-}
-
-function laps(_: number, c: number): number {
-     return _ - c * 60000;
 }
