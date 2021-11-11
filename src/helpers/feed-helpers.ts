2e8258791e95a326e022b2777ac7394a8a8023a5
punjira <saeedwmail@gmail.com>
2021-11-11 08:12:46 -0500

    add feed generator helper function

---
+++ src/helpers/feed-helpers.ts
@@ -0,0 +1,40 @@
+/**
+ * helper functions to create feed objects for all events
+ */
+
+import { Coin } from '@cryptograph-app/shared-models';
+import { Signal } from '../models/signal-model';
+
+export interface Feed {
+     message: string;
+     en_message: string;
+     location: number;
+     signal: string;
+     coin: Coin;
+     interval?: string;
+}
+
+const intervalMap: { [key: string]: string } = {
+     '1m': 'یک دقیقه ای',
+     '3m': 'سه دقیقه ای',
+     '5m': 'پنج دقیقه ای',
+     '15m': 'پانزده دقیقه ای',
+     '30m': 'سی دقیقه ای',
+     '1h': 'یک ساعته',
+     '2h': 'دو ساعته',
+     '4h': 'چهار ساعته',
+     '1d': 'یک روزه',
+};
+
+export function createP1stgFeed(signal: Signal, coin: Coin): Feed {
+     return {
+          coin: coin,
+          message: `فرصت ${
+               signal.direction === 'long' ? 'خرید' : 'فروش'
+          } جدید در نمودار ${intervalMap[signal.interval]} ${coin.name}`,
+          en_message: ``,
+          location: signal.location,
+          interval: signal.interval,
+          signal: signal.signal_key,
+     };
+}
